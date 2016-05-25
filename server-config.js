bd0e3ee505de01e63f1043585c1924f63ae2b90d
JonasMS <jonas.sota@gmail.com>
2016-05-24 18:56:09 -0700

    Insert Saved Article into DB

--- server-config.js
+++ server-config.js
@@ -25,21 +25,29 @@ app.use(express.static(__dirname + '/client'));
 //Routing
 app.post('/saveArticle', function(req, res, next) {
   //get article content
-  request(req.body.url, function( err, response, body) {
+  request(req.body.url, function(err, response, body) {
     if (err) { return res.sendStatus(500); }
 
     let data = extractor(body);
 
+    console.log('url: ', data.title);
     //replace \n with '<br>'
     // data.text = utils.sourceToHTML(data.text);
     // console.log(data.text);
+    let articleData = {
+      url: req.body.url,
+      title: data.title,
+      author: data.author,
+      data: data.date,
+      publisher: data.publisher,
+      text: data.text
+    }
 
     //save article content to db
-
+    request.post('http://localhost:3000/v1/articles', {form:articleData});
     //send file to React
     res.status(201).send(data);
   });
-
 });
 //build new document w/ article content
 //send user to new document
